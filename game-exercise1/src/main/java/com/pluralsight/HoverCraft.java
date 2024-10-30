e3b00d9433187a4bcc10d02e17a500d9305ef0f9
Damian Jerez <41656590+Djerez245@users.noreply.github.com>
2024-10-30 12:54:07 -0400

    Delete game-exercise1 directory

--- game-exercise1/src/main/java/com/pluralsight/HoverCraft.java
+++
@@ -1,5 +0,0 @@
-package com.pluralsight;
-
-public class HoverCraft extends Transportation {
-    private double hoverHeight;
-}
--- game-exercise1/src/main/java/com/pluralsight/SemiTruck.java
+++
@@ -1,7 +0,0 @@
-package com.pluralsight;
-
-public class SemiTruck extends Transportation{
-    private boolean sleeper;
-    private double height;
-    private int towingCapacity;
-}
