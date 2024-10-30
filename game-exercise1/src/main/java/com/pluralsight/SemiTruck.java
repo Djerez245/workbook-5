c10a53d15cac9120208bc887caeb5c4e9cb4446e
Damian <jerezdamian640@gmail.com>
2024-10-30 11:54:14 -0400

    created super and sub classes

---
+++ game-exercise1/src/main/java/com/pluralsight/SemiTruck.java
@@ -0,0 +1,7 @@
+package com.pluralsight;
+
+public class SemiTruck extends Transportation{
+    private boolean sleeper;
+    private double height;
+    private int towingCapacity;
+}
---
+++ game-exercise1/src/main/java/com/pluralsight/Transportation.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class Transportation {
+    protected double speed;
+    protected int numOfSeats;
+    protected int qualityOfVehicle;
+    protected String color;
+    protected double range;
+    protected boolean isElectric;
+
+
+}
+
